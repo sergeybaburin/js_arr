e943ef4510903b6bbedc97a147b4486462f197ce
baburinsy <b_sergey_u@mail.ru>
2021-11-01 01:48:10 +0200

    Copy js files into js_arr repository (baburin.s.u.@gmail.com)

---
+++ arr_1.js
@@ -0,0 +1,7 @@
+/*
+Считываение элементов массива в прямом порядке
+*/
+let arr = ["Я", "Изучаю", "JavaScript"];
+for (let i = 0; i < arr.length; i++ ) {
+console.log(arr[i]);
+}
---
+++ arr_2.js
@@ -0,0 +1,9 @@
+/*
+Считываение элементов массива в обратном порядке
+*/
+
+let arr = ["Я", "Изучаю", "JavaScript"];
+for (let i = 0; i < arr.length; i++ ) {
+let a1 = arr.length - 1;
+console.log(arr[a1 - i]);
+}
